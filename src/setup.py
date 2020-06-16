5862ad50faf98e3b7eff3eb739a3af0a074eb6c7
joan <joan.herisson@univ-evry.fr>
2020-06-16 11:08:56 +0200

    ver

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ComplexRedisDict",
-    version="0.0.5",
+    version="0.0.6",
     author="Joan Hérisson",
     author_email="joan.herisson@univ-evry.fr",
     description="Dictionnary stored in a Redis database",
